fae9e70d52cef1595fb7e995fe2b7b2307bf390d
towhid046 <towhidmorol46@gmail.com>
2025-02-04 15:41:15 +0600

    added zod validation of group cover page

---
+++ client/src/lib/groupPageSchema.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const GROUP_PAGE_SCHEMA = z.object({
+    varsityName: z.string().trim().nonempty('Select Your University'),
+    assignmentTitle: z.string().trim().nonempty('Assignment Title can not be empty'),
+    courseName: z.string().trim().nonempty('Course Name can not be empty'),
+    courseCode: z.string().trim().nonempty('Course Code can not be empty'),
+    year: z.string().trim().nonempty('Select Your Year'),
+    semester: z.string().trim().nonempty('Select Your Semester'),
+    session: z.string().trim().nonempty('Select Your Session'),
+    submissionDate: z.string().trim().nonempty('Select Your Submission Date'),
+    groupNumber: z.string().trim().nonempty('Group Number can not be empty'),
+    studentIds: z.string().trim().nonempty('Student ID can not be empty'),
+    studentDepartment: z.string().trim().nonempty('Select Your Student Department'),
+    teacherName: z.string().trim().nonempty('Teacher Name can not be empty'),
+    teacherTitle: z.string().trim().nonempty('Select Your Teacher Title'),
+    teacherDepartment: z.string().trim().nonempty('Select Your Teacher Department'),
+});
